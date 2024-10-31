b69bd0cbcb4d289edbb8ddbf04e217698dd38043
bellmann-engineering <57789413+bellmann-engineering@users.noreply.github.com>
2024-10-31 11:50:55 +0100

    Update car_example.cpp

--- Decorator/car_example.cpp
+++ Decorator/car_example.cpp
@@ -2,148 +2,132 @@
 #include <iostream>
 using namespace std;
 
-class Car // Our Abstract base class
+class Car // Abstract base class
 {
-        protected:
-                string _str;
-        public:
-                Car()
-                {
-                        _str = "Unknown Car";
-                }
- 
-                virtual string getDescription()
-                {       
-                        return _str;
-                }
- 
-                virtual double getCost() = 0;
- 
-                virtual ~Car()
-                {
-                        cout << "~Car()\n";
-                }
+public:
+    virtual string getDescription() = 0; // pure virtual function for description
+    virtual double getCost() = 0;        // pure virtual function for cost
+
+    virtual ~Car()
+    {
+        cout << "~Car()\n";
+    }
 };
- 
-class OptionsDecorator : public Car //Decorator Base class
+
+class OptionsDecorator : public Car // Decorator Base class
 {
-        public:
-                virtual string getDescription() = 0;
- 
-                virtual ~OptionsDecorator()
-                {
-                        cout<<"~OptionsDecorator()\n";
-                }
+protected:
+    Car *_b;  // pointer to the wrapped car
+public:
+    OptionsDecorator(Car *b) : _b(b) {}  // associate OptionsDecorator with a Car
+
+    virtual string getDescription() = 0;
+
+    virtual ~OptionsDecorator()
+    {
+        cout << "~OptionsDecorator()\n";
+        delete _b;  // safely delete the wrapped car
+    }
 };
- 
- 
+
 class CarModel1 : public Car
 {       
-        public:
-                CarModel1()
-                {
-                        _str = "CarModel1";
-                }
-                virtual double getCost()
-                {
-                        return 31000.23;
-                }
- 
-                ~CarModel1()
-                {
-                        cout<<"~CarModel1()\n";
-                }
+public:
+    string getDescription()
+    {
+        return "CarModel1";
+    }
+
+    double getCost()
+    {
+        return 31000.23;
+    }
+
+    ~CarModel1()
+    {
+        cout << "~CarModel1()\n";
+    }
 };
- 
- 
-class Navigation: public OptionsDecorator
+
+class Navigation : public OptionsDecorator
 {
-                Car *_b;
-        public:
-                Navigation(Car *b)
-                {
-                        _b = b;
-                }
-                string getDescription()
-                {
-                        return _b->getDescription() + ", Navigation";
-                }
- 
-                double getCost()
-                {
-                        return 300.56 + _b->getCost();
-                }
-                ~Navigation()
-                {
-                        cout << "~Navigation()\n";
-                        delete _b;
-                }
+public:
+    Navigation(Car *b) : OptionsDecorator(b) {}  // initialize with the Car pointer
+
+    string getDescription()
+    {
+        return _b->getDescription() + ", Navigation";
+    }
+
+    double getCost()
+    {
+        return 300.56 + _b->getCost();
+    }
+
+    ~Navigation()
+    {
+        cout << "~Navigation()\n";
+    }
 };
- 
-class PremiumSoundSystem: public OptionsDecorator
+
+class PremiumSoundSystem : public OptionsDecorator
 {
-                Car *_b;
-        public:
-                PremiumSoundSystem(Car *b)
-                {
-                        _b = b;
-                }
-                string getDescription()
-                {
-                        return _b->getDescription() + ", PremiumSoundSystem";
-                }
- 
-                double getCost()
-                {
-                        return 0.30 + _b->getCost();
-                }
-                ~PremiumSoundSystem()
-                {
-                        cout << "~PremiumSoundSystem()\n";
-                        delete _b;
-                }
+public:
+    PremiumSoundSystem(Car *b) : OptionsDecorator(b) {}  // initialize with the Car pointer
+
+    string getDescription()
+    {
+        return _b->getDescription() + ", PremiumSoundSystem";
+    }
+
+    double getCost()
+    {
+        return 0.30 + _b->getCost();
+    }
+
+    ~PremiumSoundSystem()
+    {
+        cout << "~PremiumSoundSystem()\n";
+    }
 };
- 
-class ManualTransmission: public OptionsDecorator
+
+class ManualTransmission : public OptionsDecorator
 {
-                Car *_b;
-        public:
-                ManualTransmission(Car *b)
-                {
-                        _b = b;
-                }
-                string getDescription()
-                {
-		        return _b->getDescription()+ ", ManualTransmission";
-                }
- 
-                double getCost()
-                {
-                        return 0.30 + _b->getCost();
-                }
-                ~ManualTransmission()
-                {
-                        cout << "~ManualTransmission()\n";
-                        delete _b;
-                }
+public:
+    ManualTransmission(Car *b) : OptionsDecorator(b) {}  // initialize with the Car pointer
+
+    string getDescription()
+    {
+        return _b->getDescription() + ", ManualTransmission";
+    }
+
+    double getCost()
+    {
+        return 0.30 + _b->getCost();
+    }
+
+    ~ManualTransmission()
+    {
+        cout << "~ManualTransmission()\n";
+    }
 };
- 
+
 int main()
 {
-        // create our Car that we want to buy
-        Car *b = new CarModel1();
+    // create our Car that we want to buy
+    Car *b = new CarModel1();
 
-        cout << "Base model of " << b->getDescription() << " costs $" << b->getCost() << "\n";  
-            
-        // let's add some more features
-            
-        b = new Navigation(b);
-        cout << b->getDescription() << " will cost you $" << b->getCost() << "\n";
-        b = new PremiumSoundSystem(b);
-        b = new ManualTransmission(b);
-        cout << b->getDescription() << " will cost you $" << b->getCost() << "\n";
+    cout << "Base model of " << b->getDescription() << " costs $" << b->getCost() << "\n";  
+        
+    // let's add some more features
+    b = new Navigation(b);
+    cout << b->getDescription() << " will cost you $" << b->getCost() << "\n";
+    
+    b = new PremiumSoundSystem(b);
+    b = new ManualTransmission(b);
+    cout << b->getDescription() << " will cost you $" << b->getCost() << "\n";
 
-        delete b;
+    delete b;
 
-        return 0;
+    return 0;
 }
